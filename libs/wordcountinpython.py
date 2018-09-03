50463999ca6b0bbf2e1d2fc3b87d2c969b9dec6c
knan02 <39821469+knan02@users.noreply.github.com>
2018-09-03 15:18:36 +0530

    new word count

--- libs/wordcountinpython.py
+++ libs/wordcountinpython.py
@@ -6,22 +6,19 @@ if __name__ == "__main__":
 
 
   parser = argparse.ArgumentParser()
-  parser.add_argument("-i", "--input_file", type = str, required=True, help = "input path for request logs")
-  parser.add_argument("-m", "--output_file", type = str, required=True, help = "input path for meta logs")
+  parser.add_argument("-i", "--input_requests", type = str, required=True, help = "input path for request logs")
+  parser.add_argument("-m", "--idemo_request", type = str, required=True, help = "input path for meta logs")
   args = parser.parse_args()    
   
-  print "input_file path={}".format(args.input_file)
-  print "output_file path={}".format(args.output_file)
   # create Spark context with Spark configuration
   conf = SparkConf().setAppName("Word count in pyhton")
   sc = SparkContext(conf=conf)
  
   
   # read in text file and split each document into words
-  words = sc.textFile(args.input_file).flatMap(lambda line: line.split(" "))
+  words = sc.textFile(args.input_requests).flatMap(lambda line: line.split(" "))
  
   # count the occurrence of each word
   wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
  
-  wordCounts.saveAsTextFile(args.output_file)
-
+  wordCounts.saveAsTextFile(args.idemo_request)
